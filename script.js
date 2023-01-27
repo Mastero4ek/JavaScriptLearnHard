4ea3073e8bd4f5f7cae9e0c57f49f54af37dd424
Chirkov Slava <slavachirkov92@gmail.com>
2023-01-27 18:32:01 +0300

    исправил усложненное задание 02

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-let num = 266219,
-	numArray = num.toString().split('');
+const num = 266219,
+	numArray = num.toString().split('');//превращаем в строку + каждый символ добавляем в массив
 
 numArray.forEach((i) => {
-	i = Number(i);
+	i = Number(i);//каждый элемент массива превращаем в число
 });
 
-let numMultiply = numArray[0] * numArray[1] * numArray[2] * numArray[3] * numArray[4] * numArray[5];
+const numMultiply = numArray.reduce((acc, rec) => acc * rec);//перемножаем каждый элемент массива
 console.log(numMultiply);
 
-let numDegree = numMultiply ** 3,
-	numDegreeArray = numDegree.toString().split('').slice(0, 2);
+const numDegree = numMultiply ** 3,//возводим в 3 степень
+	numDegreeArray = numDegree.toString().split('').slice(0, 2);//переводим в строку + каждый символ добавляем в массив + отрезаем первые два элемента
 
 numDegreeArray.forEach((i) => {
-	i = Number(i);
+	i = Number(i);//каждый элемент массива превращаем в число
 	console.log(i);
 });
