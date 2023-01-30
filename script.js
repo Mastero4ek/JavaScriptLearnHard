99bcccbe5657aa9d48cf454b1184688833f4610d
Chirkov Slava <slavachirkov92@gmail.com>
2023-01-30 21:19:36 +0300

    task03hard done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//lesson02
 const num = 266219,
 	  numArray = num.toString().split('');//превращаем в строку + каждый символ добавляем в массив
 
@@ -15,3 +16,32 @@ numDegreeArray.forEach((i) => {
 	i = Number(i);//каждый элемент массива превращаем в число
 	console.log(i);
 });
+
+//lesson03
+let lang;
+
+if(lang === 'ru') {
+	console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+} else if(lang === 'en') {
+	console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+}
+
+switch(true) {
+	case lang === 'ru':
+		console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+		break
+
+	case lang === 'en':
+		console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+}
+
+let langArr = [];
+
+langArr['ru'] = 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье';
+langArr['en'] = 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday';
+console.log(langArr[lang]);
+
+let namePerson;
+
+namePerson === 'Андрей' ? console.log('директор') : console.log('студент');
+namePerson === 'Александр' ? console.log('преподаватель') : console.log('студент');
